1cc4407b238981fd3f895bce0336f2c33345f359
Luwengier <g217881111@gmail.com>
2019-12-20 23:37:21 +0800

    Use session and flash message

--- server/app.js
+++ server/app.js
@@ -3,9 +3,19 @@ const routes = require('./routes/api/index')
 const port = process.env.PORT || 3000
 const app = new express()
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 // use middleware
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 // use router
 app.use('/api/', routes)
