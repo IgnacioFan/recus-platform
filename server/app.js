e869ccf754557b4d9dc85c0bc7ec0ea006c62c81
Luwengier <g217881111@gmail.com>
2019-12-21 10:35:09 +0800

    Install passport

--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
 const express = require('express')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const routes = require('./routes/api/index')
 const port = process.env.PORT || 3000
 const app = new express()
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 // use middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
+
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
 
 const userController = require('../../controllers/admin/userController')
 const categoryController = require('../../controllers/admin/categoryController')
+
 
 // test router
 router.get('/', (req, res) => {
@@ -18,5 +31,7 @@ router.get('/categories/:id', categoryController.getCategory)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.post('/signin', userController.signIn)
+
 
 module.exports = router
